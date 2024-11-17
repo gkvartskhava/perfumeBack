9b109a34519afcfdec0461549c838e881b4c4f1e
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-17 20:34:23 +0200

    first setup

--- perfume_api/settings.py
+++ perfume_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
