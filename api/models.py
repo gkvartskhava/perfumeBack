815445151dd52d3729fb8098d45ab6b64abd001e
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-25 20:59:22 +0200

    role models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('manager', 'Manager'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=10, choices = ROLE_CHOICES, default = 'admin')
+
+
+
+
