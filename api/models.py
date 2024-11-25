e6c70c119c50516c40ab1898c9ba57c758e61f61
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-25 21:21:47 +0200

    role models

--- api/models.py
+++ api/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('customer', 'Customer'),
-        ('manager', 'Manager'),
-        ('admin', 'Admin'),
-    )
 
-    role = models.CharField(max_length=10, choices = ROLE_CHOICES, default = 'admin')
+    ADMIN = 'admin'
+    MANAGER = 'manager'
+    CUSTOMER = 'customer'
+    ROLE_CHOICES = [
+        (ADMIN,'Administrator'),
+        (MANAGER,'manager'),
+        (CUSTOMER,'customer'),
+    ]
+
+    role = models.CharField(max_length=20, choices = ROLE_CHOICES, default = 'CUSTOMER')
 
 
 
