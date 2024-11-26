72a3ebaeddc6404592bffa759c1d0aa49d565969
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-26 22:44:29 +0200

    database

--- api/permissions.py
+++ api/permissions.py
@@ -1,15 +1,15 @@
-# from rest_framework import permissions
-# from .models import CustomUser
+from rest_framework import permissions
+from .models import CustomUser
 
-# class IsAdministrator(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return request.user.is_authenticated and request.user.role==CustomUser.ADMINISTRATOR
+class IsAdministrator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role==CustomUser.ADMIN
 
 
-# class CanDeleteReport(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return request.user.role == CustomUser.COURIER
+class CanDeleteReport(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == CustomUser.MANAGER
 
-# class CanAddReport(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return request.user.role == CustomUser.OFFICE_WORKER
+class CanAddReport(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == CustomUser.CUSTOMER
