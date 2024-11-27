b72aa4373ac5b9cc837eff4bf9b109af6eab90eb
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-27 22:10:31 +0200

    added models

--- api/models.py
+++ api/models.py
@@ -14,6 +14,24 @@ class CustomUser(AbstractUser):
 
     role = models.CharField(max_length=20, choices = ROLE_CHOICES, default = 'CUSTOMER')
 
+class Category(models.Model):
+    gender = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.gender
+
+
+class PerfumeDetails(models.Model):
+    name = models.CharField(max_length=900)
+    description = models.TextField()
+    image=models.CharField(max_length=10000)
+    image2 = models.ImageField(upload_to="images/")
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="sex")
+
+    def __str__(self):
+        return self.name
 
 
 
