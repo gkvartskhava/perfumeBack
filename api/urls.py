f47b378aefa626dfaea032de756ad6bcb87797d1
gkvartskhava <gio.kvartskhava@gmail.com>
2025-01-01 21:38:06 +0100

    search

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 
-from .views import ItemViewSet
+from .views import ItemViewSet,SearchViewSet
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 
 router.register(r'item_list',ItemViewSet, basename='item')
+router.register(r'search', SearchViewSet,basename='search')
 
 urlpatterns = [
     path('',include(router.urls))
-    path('items/', ItemViewSet),
+    
 ]
 
--- api/views.py
+++ api/views.py
@@ -6,15 +6,23 @@ from .filters import PerfumeFilter
 from rest_framework import permissions
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import ListAPIView
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = PerfumeDetails.objects.all()
     serializer_class = PerfumeDetailSerializer
     filterset_class = PerfumeFilter
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter,OrderingFilter]
     search_fields = ['name', 'category', 'price',]
 
    
+class SearchViewSet(ListAPIView):
+    queryset = PerfumeDetails.objects.all()
+    serializer_class = PerfumeDetailSerializer
+    filterset_class = PerfumeFilter
+    filter_backends = [OrderingFilter, SearchFilter]
+    search_fields = ['name', 'category', 'price',]
 
 
 
